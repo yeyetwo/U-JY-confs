3346fe05208fde30388ebf770b5239508317ee29
yeyetwo <54591954+yeyetwo@users.noreply.github.com>
2021-05-22 17:08:57 +0800

    Update udp2raw.sh

--- udp2raw.sh
+++ udp2raw.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 touch /udp2raw-tunnel/udp2raw.conf
 NewPassword=`< /dev/urandom tr -dc 0-9-A-Z-a-z-/|head -c ${1:-10}`
-echo '-c \n -k $NewPassword \n -l 0.0.0.0:4000 \n -r 43.129.240.245:0 \n --raw-mode icmp' > /udp2raw/udp2raw.conf
+echo '-c \n -k $NewPassword \n -l 0.0.0.0:4000 \n -r 43.129.240.245:0 \n --raw-mode icmp' > /udp2raw-tunnel/udp2raw.conf
 echo "password: $NewPassword"
